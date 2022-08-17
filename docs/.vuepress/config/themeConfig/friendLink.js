09d2e6138962859d166b3a3bfecd4247f3cb0964
姜忠良 <codenewman@qq.com>
2022-08-17 20:11:27 +0800

    update

---
+++ docs/.vuepress/config/themeConfig/friendLink.js
@@ -0,0 +1,43 @@
+// friendLink
+module.exports = [
+  // {
+  //   "title": "午后南杂",
+  //   "desc": "Enjoy when you can, and endure when you must.",
+  //   "link": "https://www.recoluan.com/"
+  // },
+  // {
+  //   "title": "Evan's blog",
+  //   "desc": "Web前端技术博客，积跬步以至千里，致敬每个爱学习的你。",
+  //   "link": "https://xugaoyi.com/"
+  // }
+  {
+    title: "卡夫卡的岛上书店",
+    desc: "standing on the shoulders of giants",
+    link: "http://kaimo313.gitee.io/blogs/",
+  },
+  {
+    title: "Hyouka",
+    desc: "菜鸟我是认真的",
+    link: "https://hyoukam.github.io/",
+  },
+  {
+    title: "飞鸟",
+    desc: "一只平凡的鸟罢了",
+    link: "https://lzxjack.top/",
+  },
+  {
+    title: "Hermoso",
+    desc: "时间如一捧流沙,缓缓的在指间流尽",
+    link: "https://imszz.com/",
+  },
+  {
+    title: "lzp的个人网站 ",
+    desc: "唯有热爱，可抵岁月漫长",
+    link: "https://www.liuzepeng.com",
+  },
+  {
+    title: "Ethan.Tzy ",
+    desc: "古今之成大事者，不惟有超世之才，亦必有坚忍不拔之志",
+    link: "https://tzy1997.com/",
+  },
+];
